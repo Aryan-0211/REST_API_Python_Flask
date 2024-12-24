f41c2d5b4a9735e2d01098ebeced7064c07a0d66
Aryan Gadhavi <aryangadhavi79@gmail.com>
2024-12-24 15:47:24 +0530

    Created Stores and items

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -14,9 +14,40 @@ stores = [
     }
 ]
 
-@app.route("/store", methods=["GET"])
-def get_store():
+@app.get("/store")
+def get_all_stores():
     return {"stores": stores}  # Visit http://127.0.0.1:5001/store
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post("/store/<string:name>/item")
+def add_item_to_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>")
+def get_store_by_name(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_items_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)  # Use a different port
